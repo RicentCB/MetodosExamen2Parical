d44ea940d05f6e7417a39bcb591784cfd13f4001
RicentCB <ricardo.cb009@gmail.com>
2020-12-06 17:35:46 +0100

    Add. Observacion aleatario de x y y

--- engine/temp.py
+++ engine/temp.py
@@ -12,10 +12,6 @@ Sujeto a:
 0<=x<=10 ---- Lim a 0 <= x <= 10
 1<=y<=5  ---- Lim b 1 <= y <= 5
 '''
-def evulateFunction(x: float, y: float):
-    #Funcion que evalua la ecpresion matematica 
-    return 3 * math.log(y)
-
 #Variables de minimos y maximos (limites), obtenidas de en las restricciones
 minResX = 0
 maxResX = 10
@@ -27,12 +23,30 @@ medY = ((maxResY - minResY)/2) + minResY
 
 sigmaX = (maxResX - minResX)/6
 sigmaY = (maxResY - minResY)/6
+#Funciones axiliares para la evaluacion
+def evulateFunction(x: float, y: float):
+    #Funcion que evalua la ecpresion matematica dada por el problema
+    return (2 * math.cos(x)) + 3 * math.log10(y)
 
-#Calculo de la interacion 0
-zc0 = evulateFunction(medX, medY)
+def evaluateRestrictions(x: float, y:float):
+    #Funcion que verifica el cumplimiento de las restircciones
+    return
+    (x>=minResX) and (x<=maxResX) and (y<=minResY) and (y<=maxResY)
 
-print(zc0)
-# print(invrand)
+#Funcion main
+if __name__ == "__main__":
+    #Calculo de la interacion 0
+    zc0 = evulateFunction(medX, medY)
+    al1 = 0.0972414582262059
+    al2 = 0.125047331544003
+    obsAlx = norm.ppf(al1, 0, sigmaX)
+    obsAly = norm.ppf(al2, 0, sigmaY)
+    valx = 
+
+    print(zc0)
+    print(obsAlx)
+    print(obsAly)
+
 
 
 
