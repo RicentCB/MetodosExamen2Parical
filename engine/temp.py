7752b6095d6e6f58955e2f3e5b0cb7080bc4ea2d
RicentCB <ricardo.cb009@gmail.com>
2020-12-06 16:40:17 +0100

    Feat. Numeros aleatorios, y porbabilidad inversa

--- engine/temp.py
+++ engine/temp.py
@@ -1,1 +1,16 @@
-print("hello")
+import numpy
+from scipy.stats import norm
+
+#Algoritmo de templado simulado
+
+def generateRandom():
+    return abs(numpy.random.normal(0.5,1.0)*numpy.random.rand(1))
+
+randomnum = generateRandom()
+invrand = norm.ppf(randomnum)
+
+print(randomnum)
+print(invrand)
+
+
+
