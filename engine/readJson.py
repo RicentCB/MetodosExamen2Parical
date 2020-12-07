316c06860eff97f6190c3d83a31c1def5c871db4
RicentCB <ricardo.cb009@gmail.com>
2020-12-07 10:05:26 +0100

    Add. Se introduce numero de poblacion e interaciones como argumentos

--- engine/readJson.py
+++ engine/readJson.py
@@ -1,11 +1,15 @@
 import sys
+from Templado import ATS
 
-if(len(sys.argv) > 1):
-    lenArray = len(sys.argv) - 1
-    arrCordsVal = list()
-    for i in range(0, int(lenArray/2)):
-        arrCordsVal.append((sys.argv[(2*i)+1], sys.argv[(2*i)+2]))
-    print(arrCordsVal)
+if __name__ == "__main__":
+    if(len(sys.argv) > 1):
+        lenArray = len(sys.argv) - 1
+        arrCordsVal = list()
+        for i in range(0, int(lenArray/2)):
+            arrCordsVal.append((float(sys.argv[(2*i)+1]), float(sys.argv[(2*i)+2])))
+        # print(arrCordsVal)
+        # A = ATS([(0.2, 0.1), (0.5,0.83), (0.9, 0.4)])
+        A = ATS(arrCordsVal)
 
-else:
-    print("Falta arugmetnos")
+    else:
+        print("Faltan argumentos")
--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const { PythonShell } = require("python-shell");
 var path = require("path");
 //Funcion que Incializa el arreglo de Automatas
 var options = {
-    // mode: 'json',
+    mode: 'json',
     pythonOptions: ['-u'],
     scriptPath: path.join(__dirname, 'engine/'),
     args: []
@@ -16,7 +16,17 @@ $(document).ready(function() {
         //
     $("#calcBtn").click(function(e) {
         e.preventDefault();
-        arrValidateCoords = []
+        let arrArgumetns = ["I500000", "P10"];
+        //Verificar numero de interaciones y poblacion
+        let numInter = $("input#inter").val();
+        let numPob = $("input#pob").val();
+        if (numInter !== '') {
+            arrArgumetns[0] = "I" + numInter;
+        }
+        if (numPob !== '') {
+            arrArgumetns[1] = "P" + numPob;
+        }
+        //Obetner coordenadas
         arrXVal[0] = $(".inputNumber#valx1").val();
         arrYVal[0] = $(".inputNumber#valy1").val();
         arrXVal[1] = $(".inputNumber#valx2").val();
@@ -29,47 +39,66 @@ $(document).ready(function() {
         arrYVal[4] = $(".inputNumber#valy5").val();
         for (let index = 0; index < 5; index++) {
             if (arrXVal[index] !== '' && arrYVal[index] !== '') {
-                arrValidateCoords.push(parseFloat(arrXVal[index]))
-                arrValidateCoords.push(parseFloat(arrYVal[index]))
+                arrArgumetns.push(parseFloat(arrXVal[index]))
+                arrArgumetns.push(parseFloat(arrYVal[index]))
             }
         }
-        console.log(arrValidateCoords);
-        //
-        options.args = arrValidateCoords
+        console.log(arrArgumetns);
+        //Mostrar mensaje
+        let resultContainer = $("#result-container");
+        resultContainer.fadeOut();
+        $("#process").fadeIn(500);
+        //Preparar script de python
+        options.args = arrArgumetns
         let PythonScript = new PythonShell('readJson.py', options);
         //Llamaos al script
         PythonScript.on('message', function(jsonString) {
-            console.log(jsonString)
-                //Crear grafica
-            var myPlot = document.getElementById('graphContent');
-            var xy = new Float32Array(arrValidateCoords);
+            console.log(jsonString);
+            if (jsonString.S.length == 2) {
+                var m = jsonString.S[0];
+                var b = jsonString.S[1];
+                //Html final
+                let html = "f(x)<sub>1</sub> = " + m + " x + " + b;
+                resultContainer.find("h3#lineal").html("").html(html);
+            } else {
+                var m = Math.max.apply(null, arrXVal);
+                var k = jsonString.S[0];
+                //Html final
+                let html = "f(x)<sub>2</sub> = e<sup>-" + k + "(x - " + m + ")<sup>2</sup></sup>"
+                resultContainer.find("h3#exp").html("").html(html);
+                //Ocultar Mensaje
+                $("#process").fadeOut(500);
+                //Mostrar resultado
+                resultContainer.fadeIn(500);
+            }
 
-            data = [{ xy: xy, type: 'pointcloud' }];
-            layout = {
-                title: "Grafica Generada",
-                xaxis: {
-                    type: "linear",
-                    range: [
-                        Math.min(arrXVal) - 2,
-                        Math.max(arrXVal) + 2
-                    ],
-                    autorange: false
-                },
-                yaxis: {
-                    type: "linear",
-                    range: [
-                        Math.min(arrYVal) - 2,
-                        Math.max(arrYVal) + 2
-                    ],
-                    autorange: false
-                },
-                // height: 598,
-                // width: 1080,
-                autosize: true,
-                showlegend: false
-            };
+            //Crear grafica
+            // var myPlot = document.getElementById('graphContent');
+            // var xy = new Float32Array(arrValidateCoords);
 
-            Plotly.newPlot('graphContent', data, layout);
+            // data = [{ xy: xy, type: 'pointcloud' }];
+            // layout = {
+            //     title: "Puntos en la grafica",
+            //     xaxis: {
+            //         type: "linear",
+            //         range: [
+            //             Math.min(arrXVal) - 2,
+            //             Math.max(arrXVal) + 2
+            //         ],
+            //         autorange: false
+            //     },
+            //     yaxis: {
+            //         type: "linear",
+            //         range: [-0.5, 1.5],
+            //         autorange: false
+            //     },
+            //     // height: 598,
+            //     // width: 1080,
+            //     autosize: true,
+            //     showlegend: false
+            // };
+
+            // Plotly.newPlot('graphContent', data, layout);
         });
     });
 
